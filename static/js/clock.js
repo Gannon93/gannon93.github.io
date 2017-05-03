c2074ad4ed4c56bb97f8a59df5580577ad117218
Zachary Gannon <zachgannon93@gmail.com>
2017-05-03 14:21:39 -0400

    Initial commit v2.

---
+++ static/js/clock.js
@@ -0,0 +1,22 @@
+function updateClock(){
+    var currentTime = new Date ( );
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
+
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = (currentMinutes < 10) ? "0" + currentMinutes : currentMinutes;
+
+    // Convert an hours component of "0" to "12"
+    currentHours = (currentHours < 10) ? "0" + currentHours : currentHours;
+
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes;
+
+    $("#clock").html(currentTimeString);
+
+}
+
+$(document).ready(function(){
+    setInterval('updateClock()', 100);
+});
